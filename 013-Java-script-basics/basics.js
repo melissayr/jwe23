384d0e1a62f5439678c7d92b1ab9180685b92dcd
Melissa <reim.melissa.y@gmail.com>
2023-12-12 19:53:36 +0100

    JavaScript basics
    startet basics

---
+++ 013-Java-script-basics/basics.js
@@ -0,0 +1,21 @@
+console.log("hier wird debugged!");
+
+let myName = "Melissa1234234"; //Zeichenkette:              String
+let myAge = 28; // Zahl (Ganzzahl)                          int oder integer
+let myWeight = 65.2; // Zahl (Dezimalzahl)                  float
+let organicFood = false; // Boolsche Werte (falsch/wahr)    bool
+
+let myList = ["Brot", "Milch", "Ketchup"]; // Liste/Array   array
+
+let dynamischesHTML = '<p style="color: red;">Roter Text</p>';
+
+let dynamischesHTML2 = '<p style="color: green">Grüner Text</p>';
+
+
+
+document.body.innerHTML = dynamischesHTML;
+
+let showMyListInConsole = function () {
+    console.log(myList);
+};
+
