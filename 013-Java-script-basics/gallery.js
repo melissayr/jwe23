a0221f8deb3a501bde80988c8c37ab6a15af29c2
Melissa Reim <reim.melissa.y@gmail.com>
2024-01-11 18:17:55 +0300

    Homework JS Loop  img

---
+++ 013-Java-script-basics/gallery.js
@@ -0,0 +1,14 @@
+let htmlOutput = "";
+
+
+
+for (let i = 0; i < 21; i++) {
+   htmOutput  += <a href={}></a> + "<br>";
+  }
+
+ 
+document.querySelector("#gallery").innerHTML = htmlOutput;
+
+Fancybox.bind("[data-fancybox]", {
+    // Your custom options
+});
