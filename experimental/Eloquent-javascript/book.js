bee22f78f1d0d389097f2cc0a99b288603664e2e
Melissa Reim <reim.melissa.y@gmail.com>
2024-01-14 15:05:50 +0300

    Eloquent Javascript book
    Home exercise

---
+++ experimental/Eloquent-javascript/book.js
@@ -0,0 +1,17 @@
+
+
+
+
+//Exercise  Loops
+
+let number = 0;
+while (number <= 12) {
+    console.log(number);
+    number = number + 2;
+}
+
+
+//Exercise if and else 
+
+if (1 + 1 == 2);
+consolge.log("It´s true");
