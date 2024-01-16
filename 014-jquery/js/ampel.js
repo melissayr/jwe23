802dd00b24ef792be1b1266ce38f11bcb518381a
Melissa <reim.melissa.y@gmail.com>
2024-01-16 18:12:46 +0100

    Started new einkaufsliste mit jquery

--- 014-jquery/js/ampel.js
+++ 014-jquery/js/ampel.js
@@ -38,64 +38,57 @@
 //     $("ampel").text(settings.state);
 // }, 1000);
 
-
-
-
-
-
 //Settings SELBSTVERSUCH
 
+const settings = {
+    stages: ["redlight", "orangelight", "redorangelight", "greenlight"],
 
-// const settings = {
-//     stages: ["redlight", "orangelight", "redorangelight", "greenlight"],
+    stage: "greenlight", // starten mit grün
 
-//     stage: "greenlight", // starten mit grün
+    duration: {
+        redlight: 5,
+        greenlight: 5,
+    },
 
-//     duration: {
-//         redlight: 5,
-//         greenlight: 5,
-//     },
+    switchpase: { phase: 1 },
+};
 
-//     switchpase: { phase: 1 },
-// };
+//JS
 
-// //JS
+$("#ampel").attr("class", "stage-" + settings.stage);
 
-// $("#ampel").text(settings.stage);
+const fromGreentoRed = function () {
+    // von grün auf rot
+    window.setTimeout(function () {
+        settings.stage = settings.stages[1]; // setzen auf gelb
+        window.setTimeout(function () {
+            settings.stage = settings.stages[0];
+            //warten bis die andere Funktion aufgerufen wird
+            window.setTimeout(
+                fromGreentoRed,
+                settings.duration.redlight * 1000
+            ); // setzen auf rot
+        }, 1000);
+    }, 1000);
+};
 
-// const fromGreentoRed = function () {
-//     // von grün auf rot
-//     window.setTimeout(function () {
-//         settings.stage = settings.stages[1]; // setzen auf gelb
-//         window.setTimeout(function () {
-//             settings.stage = settings.stages[0];
-//             //warten bis die andere Funktion aufgerufen wird
-//             window.setTimeout(
-//                 fromGreentoRed,
-//                 settings.duration.redlight * 1000
-//             ); // setzen auf rot
-//         }, 1000);
-//     }, 1000);
-// };
+const fromRedtoGreen = function () {
+    //von rot auf rotOrange auf grün
+    window.setTimeout(function () {
+        settings.stage = settings.stages[2]; //redorange
+        window.setTimeout(function () {
+            settings.stage = settings.stages[3];
+            window.setTimeout(
+                fromRedtoGreen,
+                settings.duration.greenlight * 1000
+            ); // setzen auf grün
+        }, 1000);
+    }, 1000);
+};
 
-// const fromRedtoGreen = function () {
-//     //von rot auf rotOrange auf grün
-//     window.setTimeout(function () {
-//         settings.stage = settings.stages[2]; //redorange
-//         window.setTimeout(function () {
-//             settings.stage = settings.stages[3];
-//             window.setTimeout(
-//                 fromRedtoGreen,
-//                 settings.duration.greenlight * 1000
-//             ); // setzen auf grün
-//         }, 1000);
-//     }, 1000);
-// };
+window.setInterval(function () {
+    //document.querySelector (#ampel).setAttribute ..... 
+    $("#ampel").attr("class", "stage-" + settings.stage);
+}, 1000);
 
-// window.setInterval(function () {
-//     $("#ampel").text(settings.stage);
-// }, 1000);
-
-
-
-
+fromGreentoRed();
