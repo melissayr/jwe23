943259dc4deee6bd3ef201e18411cedd0e9e436b
Melissa <reim.melissa.y@gmail.com>
2024-01-18 21:04:43 +0100

    Extension shopping list

--- 014-jquery/einkaufsliste-jquery.js
+++ 014-jquery/einkaufsliste-jquery.js
@@ -3,7 +3,7 @@ let myList = ["Senf", "Brot", "Butter"];
 //Wenn das Cookie für die "product-list" existiert, dann befülle das Array mit den Produkten aus dem Cookie
 if (typeof Cookies.get("product_list") !== "undefined") {
     myList = Cookies.get("product_list").split(",");
-};
+}
 
 /*Checkbox Vorlage Listenelement
 
@@ -19,12 +19,21 @@ if (typeof Cookies.get("product_list") !== "undefined") {
 //     console.log(product);
 // });
 
-const addNewProduct = function () {
-    myList.push($("#new-product").val());
-    Cookies.set("product_list", myList, {
-        expires: 365,
-    }); /*einkaufsliste gespeichert in Cookies für 365 T*/
-    prependNewProduct(myList.lenght - 1, myList[myList.length - 1]);
+    const addNewProduct = function () {
+    let value = $("#new-product").val();
+
+    if (!filteredList.length){
+        myList.push(value);
+        Cookies.set("product_list", myList, {
+            expires: 365,
+        }); /*einkaufsliste gespeichert in Cookies für 365 T*/
+        prependNewProduct(myList.lenght - 1, myList[myList.length - 1]);
+
+    } else {
+        $('#new-product').val("").focus();
+    }
+
+    
     $("#new-product").val("").focus();
 };
 
@@ -68,3 +77,22 @@ createProductList();
 // myList.forEach(function(product){
 //     console.log(product);
 // });
+
+const showFilteredList = function (list) {
+    $("#product-list").empty();
+
+    $(list).each(prependNewProduct);
+};
+
+const filterList = function () {
+    let value = $(this).val().toLowerCase(); //documentQueryselector. # new-product = this
+    let filteredList = myList.filter (function (article) {
+        return article.toLowerCase().includes(value);
+    })};
+
+
+$("#new-product").on("click", addNewProduct);
+$("#new-product").on("keyup", function (e) {
+
+    showFilteredList(filteredList);
+});
