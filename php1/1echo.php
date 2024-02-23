ab21863ae5f85ae00f9e78ea82efde06a6f3d7ec
Melissa Reim <reim.melissa.y@gmail.com>
2024-02-23 15:51:31 +0100

    PHP 1
    Startet PHP lesson

---
+++ php1/1echo.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Echo mit PHP </title>
+</head>
+<body>
+
+<!--HTML Kommenar sichtbar in console --> 
+
+<h1>Echo mit PHP</h1>
+
+<?php 
+
+//Erste Ausgabe mit PHP 
+echo "Hallo Welt!<br/>" ; 
+
+/* Mehrzeiliger 
+Kommentar
+*/
+echo "Hallo ";
+
+echo "Welt!"; ?>
+   
+</body>
+</html>
---
+++ php1/2variablen.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variablen mit PHP</title>
+</head>
+<body>
+
+<h1>Variablen mit PHP</h1>
+    
+
+<?php 
+
+
+//Ganzzahl (integer) definieren
+$alter = 28;
+
+echo "Ich bin $alter Jahre alt <br/>";
+
+//Kommazahl (float) definieren und ausgeben
+$kontostand = 9.81;
+
+echo $kontostand;
+
+
+
+
+
+?>
+
+</body>
+</html>
