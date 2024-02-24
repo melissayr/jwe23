506f55cdffd6b2dac9fc02767da62b885e6d4597
Melissa Reim <reim.melissa.y@gmail.com>
2024-02-24 11:51:57 +0100

    PHP Functions
    Count, array_rand, array_uniqe, in_array, asort, sort, array_push

--- php1/5funktionen.php
+++ php1/5funktionen.php
@@ -26,10 +26,51 @@
     echo"</pre>";
 
 
+    //HTML-Tags aus einem String entfernen
+
+    $text = "Das ist <strong>fett</strong> und <em>kursiv</em>.";
+    echo strip_tags ($text, "<strong>") ; // strong ist erleubt und em wurde entfernt
+
+    echo "<br/>";
+
+    //Länge eines String zählen zb Passwort prüfung in if abfrage (zb länge mind. 8)
+    echo strlen ($text);
+    echo "<br/>";
+    // echo mb_strlen ($text, "uft-8");
+    // echo "<br/>";
+
+
+    //Teil aus einem Text extrahieren
+    $text = "Ich bin 43 Jahre alt.";
+
+    echo substr($text, 8, 2);
+    echo "<br/>";
+    
+    //Zeilenumbrüche in <br/> umwandeln breaks 
+    $text = "Herzlich Willkommen
+    im WIFI
+    Salzburg";
+    echo nl2br($text);
+    
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
 
 
 
 
 ?>
+
 </body>
 </html>
---
+++ php1/6funktionen2.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Funktionen für Arrays</title>
+</head>
+<body>
+    <h1>Funktionen für Arrays</h1>
+
+    <?php
+    $namen = array("Peter", "Franziska", "Mario", "Katharina", "Franziska", "Christian", "Katharina", "Markus");
+
+    //Elemente (Werte) in einem Array zählen
+
+    echo count($namen);
+    echo "<br/>";
+
+    //Zufälligen Namen ausgeben
+    // echo array_rand($namen);
+    $index = array_rand($namen);
+    echo $index;
+    echo "<br/>";
+
+    //echo $namen[array_rand($namen)];
+    echo $namen[$index];
+    echo "<br/>";
+
+
+    echo"<pre>";
+    print_r($namen);
+    echo"</pre>";
+    echo count($namen); //zählen der namen im array
+
+    //Doppelte Namen entfernen mit array uniqe
+    $eindeutig = array_unique(
+        $namen
+    );
+
+    echo"<pre>";
+    print_r($eindeutig);
+    echo"</pre>";
+    echo count($eindeutig); //zählen der namen im array
+
+    // ACHTUNG INDEX MUSS NEU VERGEBEN WERDEN mit sort
+    echo "<br/>";
+    echo "<br/>";
+
+    //Prüfen ob ein Wert im Array existiert
+
+    if (in_array("Heidi", $namen)) {
+        echo "Heidi ist dabei";
+    } 
+    else {
+        echo "Heidi ist nicht dabei";
+    } 
+
+    echo "<br/>";
+    echo "<br/>";
+
+    // Aufsteigend nach Namen sortieren
+    asort($eindeutig);
+    echo"<pre>";
+    print_r($eindeutig);
+    echo"</pre>";
+
+    echo "<br/>";
+    echo "<br/>";
+
+    //Werte Nachhinein hinzufügen
+    $eindeutig[] = "Herbert";
+    array_push($eindeutig, "Julia", "Fritz");
+    echo"<pre>";
+    print_r($eindeutig);
+    echo"</pre>";
+
+    echo "<br/>";
+    echo "<br/>";
+
+    //Sortieren und neue Indizies zuweisen
+    sort($eindeutig);
+    echo"<pre>";
+    print_r($eindeutig);
+    echo"</pre>";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    ?>
+
+    
+</body>
+</html>
