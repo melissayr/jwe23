222ba19a8ad69fd08dc1b4126040265c7a59802d
Melissa Reim <reim.melissa.y@gmail.com>
2024-02-24 14:03:16 +0100

    Functions
    Date formating from MySQL format to normal , substr, strlen,

--- php1/7eigenefunktionen.php
+++ php1/7eigenefunktionen.php
@@ -62,7 +62,51 @@
     echo date_to_newDate($datum_mysql);
 
     echo "<br/>";
-    
+    echo "<br/>";
+
+    //Datum formatieren: weitere Variante
+    function de_datum_weitere ($datum_falsch) {
+        $teile = explode('-', $datum_falsch); // explode ist splitten
+        return $teile[2] . "." . $teile[1] . "." . $teile[0];
+    }
+
+    echo de_datum_weitere($datum_mysql);
+
+    echo "<br/>";
+    echo "<br/>";
+
+    //Text nach 10 Zeichen abschneiden und "..." anhängen
+
+
+    function text_abschneiden($text_lang) {
+        if (strlen($text_lang) >= 10){
+            $text_kurz = substr($text_lang, 0, 10);
+            return $text_kurz . "...";
+        } else{
+            $text_lang;
+        }
+    }
+
+    $text = "Lorem ipsum dolor.";
+    echo text_abschneiden($text);
+
+    // function text_abschneiden($text) {
+    //     if (strlen($text) <= 10 ) {
+    //         return $text;
+    //     } 
+    //     else { substr($text, 0, 10);
+    //         return $text;
+    // }}
+
+  
+
+
+
+
+
+
+
+
 
    
 
