7dbde761eb24c3122194203ae7439e6d0e10c6ca
Melissa Reim <reim.melissa.y@gmail.com>
2024-03-01 16:09:01 +0100

    PHP lesson
    server.php

---
+++ php1/9server.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Superglobal variable: $_SERVER</title>
+</head>
+<body>
+    <h1>Superglobal variable: $_SERVER</h1>
+
+    <?php
+
+    echo "<pre>";
+    print_r($_SERVER);
+    echo "</pre>";
+    
+    
+    ?>
+
+
+    
+</body>
+</html>
