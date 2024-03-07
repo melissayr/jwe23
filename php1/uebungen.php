a8aec2b43aca3729f38d18e903fcad49edf21d75
Melissa Reim <reim.melissa.y@gmail.com>
2024-03-07 19:50:52 +0300

    Startet

---
+++ php1/uebungen.php
@@ -0,0 +1,19 @@
+<!-- numm. array -->
+<?php
+$namen = array("Katharina", "Jonathan", "Julia", "Peter");
+
+echo $namen[0] . " und " . $namen[3];
+echo "<br/>";
+
+$namen[] = "Mario"; 
+echo "<br/>";
+
+echo"<pre>";
+print_r($namen);
+echo"</pre>";
+
+
+
+
+
+?>
