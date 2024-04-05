c4de2a76ce7de70778f2798293813ec0571efdc8
Melissa Reim <reim.melissa.y@gmail.com>
2024-04-05 17:38:59 +0200

    Code Dokumentiert

--- php3bsp/kreis/Kreis.php
+++ php3bsp/kreis/Kreis.php
@@ -1,14 +1,26 @@
 <?php
 
+/**
+ * Diese Blöcke sind Beispiele für "phpDoc" /  "DocBlock"
+ * und können mit phpDocumentor verarbeitet werden. 
+ */
+
 class Kreis { 
     //Konstate, die fix einer Klasse zugeordnet ist
     const PI = 3.141592654;
 
-    private float $radius; //eigenschaft
+    private float $radius; //Eigenschaft
 
     public function __construct(float $r) { //mit float fließkommazahl! raius kann kein string sein
 
-        $this->radius = $r; //methode
+        $this->set_radius ($r); //Methode 
+    }
+
+    //Der Destruktor wird auf jeden Fall ausgeführt, wenn das Objekt gelöscht wird
+    //Die kann über unset $k durch den Programmierer passieren,
+    //oder automatisch durch PHP wenn das Programm zu Ende durchgelaufen ist.
+    public function __destruct() {
+        echo "Kreis im Radius " . $this->radius . " wurde zerstört.<br>";
     }
 
     public function flaeche(): float {
@@ -20,9 +32,20 @@ class Kreis {
         return $this->radius * 2;
     }
 
+    /** Brechnet anhand des geg. Radius den Umfang d. Kreises
+    * @return float  Der berechnete Umfang d Kreises
+    */
+
     public function umfang(): float {
         return 2 * $this->radius * self::PI ;
     }
+    /**
+     * Setzt einen neuen Radius für den Kreis. Auch der Kreis bereits existiert und mir einem Radius im Konstruktor befüllt wurde,
+     * kann man so einen neuen Radius setzen.
+     * @param int|float $neuerRadius der neue Radius der gesetzt werden soll.
+     * @return void
+     * @throws Exception
+     */
 
     public function set_radius(float $neuerRadius): void {
         if($neuerRadius <=0) {
@@ -30,6 +53,7 @@ class Kreis {
             //Gibt Kollegen einen Hinweis, was sie falsch gemacht haben (die entwickler für index.php zb)
             throw new Exception("Radius muss größer 0 sein."); //neue Ausnahme(new Exception) vordefiniert von PHP! wenn Exception geworfen wird kann der code danach nicht ausgeführt werden 
         } //einen throw(wurf) muss man auch fangen 
+
         $this->radius = $neuerRadius;
     }
 }
--- php3bsp/kreis/index.php
+++ php3bsp/kreis/index.php
@@ -16,18 +16,23 @@ $k->set_radius(5);
 echo "Durchmesser NEU: " .  $k->durchmesser();
 echo"<br>";
 
-$benutzer_eingabe = -2;
+$benutzer_eingabe = 2;
 
+//Wird in einem try-Block eine Exception geworfen, hat man mit "catch" die Möglichkeit darauf zu reagieren
 try{
-
     $k ->set_radius($benutzer_eingabe);
     echo "Durchmesser zum Schluss " . $k->durchmesser();
     echo"<br>";
 
 } catch (Exception $ex) {
+    //Fängt alle Exception-Objekte ab, die im try-Block gewrofen werden, throw new Exception ("..")
     echo "Da war was falsch: " . $ex->getMessage();
     echo"<br>";
 
-} finally { 
+} finally {  // Dieser code wird in jedem Fall ausgeführt
     echo "Das wars wohl. <br>";
 }
+
+unset($k);
+
+echo "Letzte Ausgabe<br>"; 
