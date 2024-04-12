4115ee44a7ce636da9c1907646830470c7b17e6e
Melissa Reim <reim.melissa.y@gmail.com>
2024-04-12 16:45:56 +0200

    Continued errors

--- php3projekt/admin/Fdb_Klassen/Validieren.php
+++ php3projekt/admin/Fdb_Klassen/Validieren.php
@@ -3,11 +3,30 @@ namespace WIFI\Php3\Fdb_Klassen;
 
 class validieren 
 {   
-    public function istAusgefuellt(string $wert): bool 
+
+    private array $errors = array();
+                                    //in login $_POST Parameter hängen mit diesen beiden parametern zusammen 1 und 2 / 1 und 2
+    public function ist_ausgefuellt(string $wert, string $feldname): bool 
     {
         if (empty($wert)) {
+            $this->errors[] = $feldname  . " war leer.";
             return false;
         } //else
         return true;
+    }
+
+    public function fehler_html(): string
+    {
+        if (empty($this->errors)) {
+            return "";
+        }
+
+
+
+        $ret = "<ul>";
+        foreach($this->errors as $error){
+        $ret .= "<li>" . $error . "</li>";
+        } $ret .= "</ul>";
+        return $ret;
     }
 }
