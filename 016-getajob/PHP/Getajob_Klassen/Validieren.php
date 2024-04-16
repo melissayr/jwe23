0a7f7533c4fbede37d8a03326b4b1b8d39996dc0
Melissa Reim <reim.melissa.y@gmail.com>
2024-04-16 15:31:29 +0300

    started login, validation, autoloader, mysql con.
    alles anpassen auf getajob (chaos beseitigen, debuggen)

--- 016-getajob/PHP/Getajob_Klassen/Validieren.php
+++ 016-getajob/PHP/Getajob_Klassen/Validieren.php
@@ -15,48 +15,9 @@ class Validieren
         return true;
     }
 
-    public function ist_kennzeichen(string $wert, string $feldname): bool
-    {   
-        //nach irgendeinem Kennzeichen suchen, das NICHT ^ A-Z, 0-9, oder Bindestrich ist.
-        if (preg_match("/[^A-Z0-9\-]/i", $wert)){ // alles was NICHT diese Zeichen sind, ist ein Fehler zb Sonderzeichen usw.
-            $this->errors[] = "Im " . $feldname  . " sind nur Buchstaben, Zahlen und Minus erlaubt.";
-            return false;
-        }
-        //auf korrekte Länge prüfen
-        if(strlen($wert) > 8 || strlen($wert) < 3){
+    // LOGIN VALIDIEREN
 
-        $this->errors[] = "Die Länge von " . $feldname  . " scheint falsch zu sein.";
-            return false;
-        }
-
-        return true; //wenn er in die IF drüber nicht reingegangen ist, ist alles ok 
-    }
-
-
-
-
-
-    public function ist_jahr(string $wert, string $feldname): bool
-    {   //muss eine zahl sein
-        if(!is_numeric($wert)) {
-            $this->errors[] = "Im " . $feldname  . " dürfen nur Zahlen verwendet werden.";
-            return false;
-        }
-
-        //auf korrekte Länge prüfen - Jahr kann nur 4 zahlen sein
-        if(strlen($wert) != 4 ){ //  ungleich 4   oder strlen($wert) > 4 || strlen($wert) < 4
-
-            $this->errors[] = "Die Länge von " . $feldname  . " scheint falsch zu sein.";
-            return false;
-        }
-
-        if (date("Y") < $wert || $wert < 1890) {
-            $this->errors[] = "Das " . $feldname  . " kann nicht in der Zukunft liegen und muss größer als 1890 sein.";
-            return false;
-        }
-
-        return true; //wenn er in die IF drüber nicht reingegangen ist, ist alles ok 
-    }
+    //HINTERHER JOBS UND KATEGORIE VALIDIEREN
 
 
 
