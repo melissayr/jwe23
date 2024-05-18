9cc3bfa85537285ae38c3a3ca7eb4a278106d5b8
melissayr <reim.melissa.y@gmail.com>
2024-05-18 11:28:48 +0200

    Finished delete

--- 016-getajob/PHP/admin/jobs_entfernen.php
+++ 016-getajob/PHP/admin/jobs_entfernen.php
@@ -4,16 +4,11 @@ ist_eingeloggt();
 
 
 echo "<h1> Job löschen</h1>";
-  
+
+$sql_id = escape($_GET["id"]);
+
 
 if(!empty($_GET["doit"])){
-
-
-
-$sql_id = escape($_POST["id"]);
-$sql_titel = escape($_POST["titel"]);
-$sql_jobs = escape( $_POST["jobs"]);
-$sql_kategorie_id = escape( $_POST["kategorie_id"]);
 
     //Bestätigungslink wurde geklickt -> wirklich in DB löschen
 
@@ -23,22 +18,24 @@ $sql_kategorie_id = escape( $_POST["kategorie_id"]);
 
 else {
     //Benutzer fragen , ob der Job wirklich entfert werden soll
-    $result = query("SELECT * FROM jobs WHERE id= '{$sql_id}'");
+    $result = query("SELECT * FROM jobs WHERE id = '{$sql_id}'");
     $row = mysqli_fetch_assoc($result);
 
 
 
-if (empty ($row)) {
-    // echo "<p> 
-    // Sind sie sich sicher, das sie den Job <strong>". htmlspecialchars($row["titel"]). "</strong> entfernen möchten?</p>";
-    
-    //     } else {
-    //Job existiert nicht
-    echo "<p> Job existiert nicht (mehr). <a href='jobs_liste.php'>Zurück zur Job Liste</a></p>";
+    if (!empty ($row)) {
+        echo "<p> 
+        Sind sie sich sicher, das sie den Job <strong>". htmlspecialchars($row["titel"]). "</strong> entfernen möchten?</p>"
+        .
+        "<p> <a href='jobs_liste.php'> Nein abbrechen</p></a>" 
+        .
+        "<p><a href='jobs_entfernen.php?id={$row['id']}&amp;doit=1'> Ja sicher</p></a>";
+        
+        } else {
+    // Job existiert nicht
+        echo "<p> Job existiert nicht (mehr). <a href='jobs_liste.php'>Zurück zur Job Liste</a></p>";
     }
 }
 
 include "../fuss.php";
-
-
 ?>
