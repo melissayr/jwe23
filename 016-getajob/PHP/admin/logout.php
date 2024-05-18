7b509592f27f4113303e0b78a726b43414d28a6a
melissayr <reim.melissa.y@gmail.com>
2024-05-18 10:08:49 +0200

    Revert "Continued"
    This reverts commit d96212f87b9b875eafc7eff75411044781b7fe41.

--- 016-getajob/PHP/admin/logout.php
+++ 016-getajob/PHP/admin/logout.php
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 
 <!-- Link zum css -->
 <link href="style.css" rel="stylesheet" type="text/css" />
@@ -39,46 +38,4 @@ href="../vendor/bootstrap-5.3.2-dist/css/bootstrap.css"
 
 <?php
 include "../fuss.php";
-=======
-
-<!-- Link zum css -->
-<link href="style.css" rel="stylesheet" type="text/css" />
-<link
-rel="stylesheet"
-href="../vendor/bootstrap-5.3.2-dist/css/bootstrap.css"
-/>
-<?php
-
-    //LOGOUT 
-
-    session_start();
-
-    //einer dieser befehle würde reichen!!! :
-
-
-    //löscht alle Session variablen
-    unset($_SESSION["eingeloggt"]); // entscheidet ob wir eingeloggt sind oder nicht
-
-
-?>
-<!DOCTYPE html>
-<html lang="de">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Logout aus dem Jobs-Administrationsbereich</title>
-</head>
-<body>
-    <h1>Logout aus dem Jobs-Administrationsbereich</h1>
-
-    <p>Sie wurden ausgeloggt.</p>
-
-    <p><a href="login.php">Hier gehts zurück zum Login</a></p>
-    
-</body>
-</html>
-
-<?php
-include "../fuss.php";
->>>>>>> Stashed changes
 ?>
--- 016-getajob/getajob.js
+++ 016-getajob/getajob.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 // Hier sind die functions für die Hamburger navigation 
 
 $(document).ready(function() {
@@ -53,59 +52,3 @@ $(document).ready(function() {
   });
   
 
-=======
-// Hier sind die functions für die Hamburger navigation 
-
-$(document).ready(function() {
-    const menuIcon = $("#menu-icon");
-    const slideoutMenu = $("#slideout-menu");
-    const searchIcon = $("#search-icon");
-    const searchBox = $("#searchbox");
-  
-    searchIcon.click(function() {
-      if (searchBox.css("top") === '72px') {
-        searchBox.css("top", '24px');
-        searchBox.css("pointer-events", 'none');
-      } else {
-        searchBox.css("top", '72px');
-        searchBox.css("pointer-events", 'auto');
-      }
-    });
-  
-    menuIcon.click(function() {
-      if (slideoutMenu.css("opacity") === "1") {
-        slideoutMenu.css("opacity", '0');
-        slideoutMenu.css("pointer-events", 'none');
-      } else {
-        slideoutMenu.css("opacity", '1');
-        slideoutMenu.css("pointer-events", 'auto');
-      }
-    });
-  });
-  
-// Hier kommt die select job function
-
-  $(document).ready(function() {
-    // Function Eingabewert Filtern
-    $("#myInput").on("keyup", function() {
-      const input = $(this).val().toUpperCase();
-      const table = $("#myTable");
-      const tr = table.find("tr");
-  
-      // Loop läuft durch alle Tabellenzeilen und versteckt die nicht der Eingabe entsprechen
-      tr.each(function() {
-        const td = $(this).find("td:first-child");
-        if (td.length > 0) {
-          const txtValue = td.text() || td.html();
-          if (txtValue.toUpperCase().indexOf(input) > -1) {
-            $(this).show();
-          } else {
-            $(this).hide();
-          }
-        }
-      });
-    });
-  });
-  
-
->>>>>>> Stashed changes
