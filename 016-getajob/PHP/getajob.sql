9647c92eaf3bdf50c771f6a19ecd488cbab910a5
melissayr <reim.melissa.y@gmail.com>
2024-05-18 10:51:04 +0200

    SQL

--- 016-getajob/PHP/getajob.sql
+++ 016-getajob/PHP/getajob.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 11. Mai 2024 um 09:32
+-- Erstellungszeit: 18. Mai 2024 um 10:45
 -- Server-Version: 10.4.32-MariaDB
 -- PHP-Version: 8.2.12
 
@@ -47,10 +47,29 @@ INSERT INTO `benutzer` (`id`, `benutzername`, `passwort`) VALUES
 --
 
 CREATE TABLE `jobs` (
-  `id` int(11) NOT NULL,
+  `id` int(10) UNSIGNED NOT NULL,
   `jobs` varchar(255) NOT NULL,
-  `titel` varchar(50) NOT NULL
+  `titel` varchar(50) NOT NULL,
+  `kategorie_id` int(10) UNSIGNED NOT NULL,
+  `qualifikationen` varchar(255) NOT NULL,
+  `dienstort` varchar(50) NOT NULL,
+  `stundenausmaß` int(50) NOT NULL,
+  `mindestgehalt_euro` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Daten für Tabelle `jobs`
+--
+
+INSERT INTO `jobs` (`id`, `jobs`, `titel`, `kategorie_id`, `qualifikationen`, `dienstort`, `stundenausmaß`, `mindestgehalt_euro`) VALUES
+(1, 'Web Programmierung, Web Design, ', 'Web-Entwickler/in', 4, '', '', 0, 0),
+(2, 'Kinderpädagogik, Kindererziehung', 'Erzieher/in', 3, '', '', 0, 0),
+(3, 'Inneneinrichtung, Innendesign, Kunst', 'Innenarchitekt', 1, '', '', 0, 0),
+(5, 'Augen untersuchen', 'Augenarzt', 5, '', '', 0, 0),
+(16, 'Bilanzierung, Buchhaltung', 'Buchhalter/in', 2, '', '', 0, 0),
+(18, 'Administration, Planung', 'IT Systemadministrator/in', 4, '', '', 0, 0),
+(19, 'Entwickeln, Programmieren', 'Softwareentwickler/in', 4, '', '', 0, 0),
+(20, 'Heilung Therapie', 'Humanenergetik', 5, '', '', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -59,10 +78,21 @@ CREATE TABLE `jobs` (
 --
 
 CREATE TABLE `kategorien` (
-  `id` int(11) NOT NULL,
+  `id` int(10) UNSIGNED NOT NULL,
   `kategorien` varchar(255) NOT NULL,
   `sonstige` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Daten für Tabelle `kategorien`
+--
+
+INSERT INTO `kategorien` (`id`, `kategorien`, `sonstige`) VALUES
+(1, 'Bau, Architektur, Vermessung', ''),
+(2, 'Dienstleistung', ''),
+(3, 'Soziales, Pädagogik', ''),
+(4, 'IT, Computer', ''),
+(5, 'Gesundheit', '');
 
 --
 -- Indizes der exportierten Tabellen
@@ -78,13 +108,13 @@ ALTER TABLE `benutzer`
 -- Indizes für die Tabelle `jobs`
 --
 ALTER TABLE `jobs`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`) USING BTREE;
 
 --
 -- Indizes für die Tabelle `kategorien`
 --
 ALTER TABLE `kategorien`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`) USING BTREE;
 
 --
 -- AUTO_INCREMENT für exportierte Tabellen
@@ -100,13 +130,13 @@ ALTER TABLE `benutzer`
 -- AUTO_INCREMENT für Tabelle `jobs`
 --
 ALTER TABLE `jobs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT für Tabelle `kategorien`
 --
 ALTER TABLE `kategorien`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
