a3e5068ace8fc0eea9fb79fe4745af11591b88bf
Melissa Reim <reim.melissa.y@gmail.com>
2024-05-20 15:04:22 +0300

    Continued

--- 016-getajob/PHP/kopf.php
+++ 016-getajob/PHP/kopf.php
@@ -1,19 +1,11 @@
-<!DOCTYPE html>
-<html lang="de">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <title>getajob</title>
-
-</head>
-
 <!-- Google fonts -->
 <link href="https://fonts.googleapis.com/css?family=Roboto|Roboto+Condensed|Roboto+Slab" rel="stylesheet">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt"
   crossorigin="anonymous">
-
 <!-- Link zum css -->
+
+
+
 <link href="style.css" rel="stylesheet" type="text/css" />
   
 <!-- Bootstrap css -->
@@ -25,8 +17,6 @@ href="../vendor/bootstrap-5.3.2-dist/css/bootstrap.css"
 <!--Material css-->
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
-
-<body>
 
 
 <title>Get a Job Abschlussprüfung</title>
